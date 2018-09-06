29ef52122ee7b500e43ccd52454542ff7a6a7c96
Alex Kharouk <akharouk@gmail.com>
2018-09-06 12:27:39 +0100

    initial commit + birthday can be displayed

--- lib/birthday_list.rb
+++ lib/birthday_list.rb
@@ -1,1 +1,28 @@
+require 'date'
 
+
+class BirthdayList
+    
+    attr_reader :list
+    def initialize
+        @months = Date::MONTHNAMES
+        @list = []
+        @date = ""
+    end
+
+    def add(name, date)
+        @list << { name: name, date: date }
+    end
+
+    def display
+        # assume date looks like dd/mm/yyyy
+        day, month, year = @list[0][:date].split("/")
+
+        p "#{@list[0][:name]}'s birthday is on the #{day} of #{@months[month.to_i]}, #{year}"
+
+    end 
+end 
+
+birthday = BirthdayList.new 
+birthday.add("Jon", "12/03/2322")
+birthday.display
--- spec/birthday_list_spec.rb
+++ spec/birthday_list_spec.rb
@@ -1,1 +1,19 @@
+require 'birthday_list'
 
+describe BirthdayList do
+    birthdays = BirthdayList.new
+    birthdays.add("Billy", "12/5/1770") 
+    it "Is able to add name and date" do
+        expect(birthdays.list).to eq [ { name: "Billy", date: "12/5/1770" } ]
+    end 
+
+    it "Is able to display stored names and dates, in a nice format" do
+        expect(birthdays.display).to eq "Billy's birthday is on the 12 of May, 1770."
+    end
+
+    birthdays_2 = BirthdayList.new
+    birthdays_2.add("Jonny", "3/11/2100")
+    it "Correctly displays the name and date using the nice format" do
+        expect(birthdays_2.display).to eq "Jonny's birthday is on the 3 of November, 2100."
+    end
+end 
